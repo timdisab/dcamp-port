f711c28f82cf1db99c31a4c2e5fa19faf9c8c143
Tim DiSabatino <tim@pathwaysdigital.com>
2017-07-20 10:47:29 -0700

    Implemented destroy action

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -39,4 +39,17 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.find(params[:id])
   end
 
+  def destroy
+    # Perform the lookup
+    @portfolio_item = Portfolio.find(params[:id])
+
+    # Destroy the record
+    @portfolio_item.destroy
+
+    # Redirect
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Record was successfully removed.' }
+    end
+  end
+
 end
