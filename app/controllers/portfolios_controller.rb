074efcbf5440ffb8561574683f16f2c4996e51da
Tim DiSabatino <tim@pathwaysdigital.com>
2017-09-05 14:01:45 -0700

    Updated portfolio to include attr for position

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,9 +2,9 @@ class PortfoliosController < ApplicationController
   before_action :set_portfolio_item, only: [:edit, :update, :show, :destroy]
   layout "portfolio"
   access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
-  
+
   def index
-    @portfolio_items = Portfolio.all
+    @portfolio_items = Portfolio.by_position
   end
 
   def angular
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,6 +1,6 @@
 class Portfolio < ApplicationRecord
   has_many :technologies, dependent: :destroy
-  accepts_nested_attributes_for :technologies, 
+  accepts_nested_attributes_for :technologies,
                                 reject_if: lambda { |attrs| attrs['name'].blank? }
 
   include Placeholder
@@ -8,6 +8,10 @@ class Portfolio < ApplicationRecord
 
   def self.angular
     where(subtitle: 'Angular')
+  end
+
+  def self.by_position
+    order("position ASC")
   end
 
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
